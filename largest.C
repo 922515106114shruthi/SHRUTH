dac9b5cdb8e1e603c527faca93a59c68ae333f4a
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-01-10 19:42:32 +0530

    Create largest.C

---
+++ largest.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the values of a,b&c:");
+scanf("%d,%d,%d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+{
+printf("%d is greatset",a);
+}
+else
+{
+printf("%d is greatest",c);
+}
+if(b>c)
+{
+printf(%d is greatest",b);
+}
+else
+{
+printf(%d is greatest",c);
+}
+}
+getch();
+}
+
+}
