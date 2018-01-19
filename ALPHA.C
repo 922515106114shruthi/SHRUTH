0f3da075266791b71ab7b013f89c5173d7df7c87
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-01-19 16:58:03 +0530

    Create ALPHA.C

---
+++ ALPHA.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char ch=A;
+/*	printf(" enter the alphabet:");
+	scanf("%d",&ch);*/
+	if((ch>=65&& ch<=90)||(ch>=97 && ch<=122 ))
+	{
+		printf("%c",ch);
+	}
+	else
+	{
+		printf("%c is not",ch);
+	}
+	
+		
+		
+			
+	return 0;
+}
