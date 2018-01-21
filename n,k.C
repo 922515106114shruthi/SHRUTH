be0ba8e6c93333e78cc6cfa2d6a9f3f45445339b
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-01-21 11:48:48 +0530

    Create n,k.C

---
+++ n,k.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,j,c=o,k;
+printf(" enter the value :");
+scanf("%d",&n);
+printf(" enter the k value:");
+scanf("%d",&k);
+for(i=1;i<=n;i++)
+{
+if(k==i)
+{
+for(j=0;j<=i;j++)
+{
+c=c+j;
+}
+}
+
+printf("%d,",i);
+}
+printf("%d is sum",c);
+getch();
+}
