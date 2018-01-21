89e6e5e3bc77b0473620e0d42bd8cff32ace38ea
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-01-21 10:29:18 +0530

    Update int.C

--- int.C
+++ int.C
@@ -2,17 +2,15 @@
 #include<conio.h>
 void main()
 {
-int a,n,count=1;
+int b[20],n,count=0;
 printf("enter the value");
 scanf("%d",&n);
 for(i=0;i<=n;i++)
 {
-scanf("%d",&b);
-if(b>=0)
-{
+scanf("%d",&b[i]);
 count++;
 }
-}
+
 printf("%d",count);
 getch();
 }
