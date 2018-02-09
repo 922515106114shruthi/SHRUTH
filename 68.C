5dc9269d710b1226e6a7da69b5ae6774ecc2db14
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-09 11:03:17 +0000

    Create 68.C

---
+++ 68.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf(" enter the number:");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("%d is multiple of 7",a);
+}
+else
+{
+printf("%d is not multiple of 7",a);
+}
+getch();
+}
