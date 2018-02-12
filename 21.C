46a270e163fde44c823b9545217a4c7fe949a522
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-12 16:42:07 +0530

    Create 21.C

---
+++ 21.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[20],n,i;
+printf(" enter the range:");
+scanf("%d",&n);
+printf(" enter the first range:");
+scanf("%d",&f);
+for(i=f;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=f;i<=n;i++)
+{
+add=((a[f]+a[n])*n)/2;
+}
+printf("%d",add);
+getch();
+}
