fa2d4bf3e9ac6035fbf10fa873ecac8532412708
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-19 15:58:26 +0530

    Create 25.C

---
+++ 25.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[20],i,j,n,t;
+printf(" enter the range:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=n;i++)
+{
+printf("%d",a[i]);
+}
+printf("\n median number:");
+for(i=1;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}}
+printf("%d",a[(n+1)/2]);
+}
+getch();
+}
