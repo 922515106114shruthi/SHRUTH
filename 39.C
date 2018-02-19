606ffa804c387e6f1b748306d903320630658e57
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-19 16:54:04 +0530

    Create 39.C

---
+++ 39.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+	int a[20],i,j,n,t;
+printf(" enter the range:");
+scanf(" %d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=n;i++)
+{
+printf("\n%d",a[i]);
+}
+printf(" \nmaximum number:");
+for(i=1;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+	if(a[i]>a[j]){
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}}}
+printf("\n%d",a[n]);
+getch();
+}
