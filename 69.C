c53ce36aa2334be80df93c5780219c038227e82d
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-19 16:41:45 +0530

    Create 69.C

---
+++ 69.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	{
+int a,n,m;
+printf(" enter the number of n:");
+scanf("%d",&n);
+printf(" enter the number of m:");
+scanf("%d",&m);
+a=n-m;
+if(a%2==0)
+{
+printf(" even");
+}
+else
+{
+printf(" odd");
+}
+getch();
+}
