bae7aacc66028ea2fdda1485e2ea6af9048e7c04
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-28 16:09:58 +0530

    Create 116.C

---
+++ 116.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{          
+           char n[22],k[22];
+           scanf("%s",n);
+           scanf("%s",k);           
+           printf("%s", strcat(n,k));
+           getch();
+ }          
+            
