503b888f280e14117e05c295d593bbb053047ac3
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-02-28 16:51:31 +0530

    Create 72.C

---
+++ 72.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+           char s[20];
+           printf(" enter the character:");
+           scanf("%s",s);
+           if(s>=97&&s<=122)
+           {
+           	printf("yes");
+           }else
+           {
+           	printf("no");
+           }
+           getch();
+  }         
