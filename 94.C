38b281d8bfaa4198235fb13a019da9469514ed56
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-03-02 16:58:06 +0530

    Create 94.C

---
+++ 94.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{            int n,k,a[20],i;
+            scanf("%d",&n);
+            scanf("%d",&k);
+            for(i=1;i<=n;i++)
+            {
+            
+            	scanf("%d",&a[i]);
+            }
+             for(i=1;i<=n;i++)
+            {
+            	printf("%d",a[i]);
+              
+            }
+            for(i=1;i<=n;i++)
+            {
+            	if(k==i)
+            	{
+            		printf("\n%d",a[i]);
+            	}
+}
+            
+        getch();
+}
